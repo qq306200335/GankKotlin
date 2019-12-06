5f5669700ba063e17005a834a11a40edc5e1acb7
baiyunfei <0306200335@qq.com>
2019-12-06 10:23:28 +0800

    1.更新环境配置

--- libview/src/main/java/com/xiaobai/libview/chart/LineChartView.kt
+++ libview/src/main/java/com/xiaobai/libview/chart/LineChartView.kt
@@ -7,6 +7,7 @@ import android.util.AttributeSet
 import android.util.TypedValue
 import android.view.MotionEvent
 import android.view.View
+import com.kzcpm.commonview.chart.ChartUtil
 import com.xiaobai.libutils.common.DensityUtils
 import com.xiaobai.libview.R
 
